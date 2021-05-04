6103a70654e92863884e035e14cc1b9ee92345f4
Mohammad <darbanhandrew@gmail.com>
2021-05-04 18:01:15 +0430

    before material

--- woosync/admin.py
+++ woosync/admin.py
@@ -1,15 +1,39 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from admin_actions.admin import ActionsModelAdmin
 # Register your models here.
-from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 
 from woosync.models import Customer, Order
+from woosync.smshelper import send_sms
 from woosync.woocommerce import sync_orders
 
 
-class CustomerAdmin(admin.ModelAdmin):
-    pass
+class CustomerAdmin(ActionsModelAdmin):
+    actions = ['send_sms']
+
+    def send_sms(self, request, queryset):
+        if 'apply' in request.POST:
+            # The user clicked submit on the intermediate form.
+            # Perform our update action:
+            # queryset.update(status='NEW_STATUS')
+
+            # Redirect to our admin view after our update has
+            # completed with a nice little info message saying
+            # our models have been updated:
+            messages.success(request, 'SMS Sent')
+            sms_text = request.POST['sms_text']
+            selected_action = request.POST['_selected_action']
+            for customer in queryset:
+                 send_sms(sms_text, customer)
+            print(selected_action)
+            return HttpResponseRedirect(request.get_full_path())
+        return render(request,
+                      'admin/send_sms.html',
+                      context={'customers': queryset})
+
+    send_sms.short_description = "Send sms"
 
 
 class OrderAdmin(ActionsModelAdmin):
---
+++ woosync/smshelper.py
@@ -0,0 +1,23 @@
+from .melipayamak import Api
+from .models import Customer
+
+username = 'gaatgallery'
+password = '2488'
+api = Api(username, password)
+
+
+def send_sms(text, customer):
+    # customer = Customer.objects.get(id=customer_id)
+    if customer:
+        text = text.replace("!!first_name!!",customer.billing_first_name)
+        text = text.replace("!!last_name!!", customer.billing_last_name)
+        text = text.replace("!!address_1!!", customer.billing_address_1)
+        text = text.replace("!!city!!", customer.billing_city)
+        text = text.replace("!!state!!", customer.billing_state)
+        text = text.replace("!!phone!!", customer.billing_phone)
+        text = text.replace("!!postcode!!", customer.billing_postcode)
+        sms = api.sms()
+        to = '09372611685'
+        _from = '30008666971367'
+        # text = 'تست وب سرویس ملی پیامک'
+        response = sms.send(to, _from, text)
